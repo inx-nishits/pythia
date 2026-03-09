759ae8484660746510c4f94b29a24efdefd4909c
hirenm-bit <hiren.m@inheritx.com>
2026-03-09 12:29:40 +0530

    Add google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Instrument_Sans, Roboto } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const instrumentSans = Instrument_Sans({
@@ -25,6 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) / GA4 */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LY8FBGTQ4T"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LY8FBGTQ4T');
+          `}
+        </Script>
+      </head>
       <body className={`${instrumentSans.variable} ${robotoFont.variable} antialiased`}>
         {children}
       </body>
